8f857ba6ef7ee4e42656a12f8fc411092d35af4c
Jake Sweeney <jake.d.sweeney@gmail.com>
2023-10-18 14:57:41 -0500

    centered warnings and added color to warning

--- health.js
+++ health.js
@@ -14,18 +14,23 @@ function updateDisplayedImage(temperature, humidity) {
 		const humType = document.getElementById("humType");
 		const humDes = document.getElementById("humDes");
 		const humid = document.getElementById("humid");
+		const humVal = document.getElementById("humVal");
 		let showCom = true;
 		if (humidity > 60) {
 			humImage.src = health.wet.src;
 			humType.textContent = health.wet.type;
+			humType.style.color = "#6d0907"
 			humDes.textContent = health.wet.description;
 			humid.style.display = "block";
+			humVal.textContent = `Humidity: ${humidity}`;
 			showCom = false;
 		} else if (humidity < 40) {
 			humImage.src = health.dry.src;
 			humType.textContent = health.dry.type;
+			humType.style.color = "#6d0907"
 			humDes.textContent = health.dry.description;
 			humid.style.display = "block";
+			humVal.textContent = `Humidity: ${humidity}`;
 			showCom = false;
 		}
 		else {
@@ -36,17 +41,22 @@ function updateDisplayedImage(temperature, humidity) {
 		const tempType = document.getElementById("tempType");
 		const tempDes = document.getElementById("tempDes");
 		const temp = document.getElementById("temp");
+		const tempVal = document.getElementById("tempVal");
 		if (temperature > 75) {
 			tempImage.src = health.hot.src;
 			tempType.textContent = health.hot.type;
+			tempType.style.color = "#6d0907";
 			tempDes.textContent = health.hot.description;
 			temp.style.display = "block";
+			tempVal.textContent = `Temp: ${temperature}`;
 			showCom = false;
 		} else if (temperature < 66) {
 			tempImage.src = health.cold.src;
 			tempType.textContent = health.cold.type;
+			tempType.style.color = "#6d0907";
 			tempDes.textContent = health.cold.description;
 			temp.style.display = "block";
+			tempVal.textContent = `Temp: ${temperature}`;
 			showCom = false;
 		}
 		else {
